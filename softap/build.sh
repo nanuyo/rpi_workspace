351e2bc29eb2e2fc4d804330583c555525a2f741
HJ Park <hjparkg71@gmail.com>
2024-05-14 17:53:52 +0900

    dsada

---
+++ softap/build.sh
@@ -0,0 +1,1 @@
+gcc -o softap_firefly3288 softap_firefly3288.c
--- softap/softap_firefly3288.c
+++ softap/softap_firefly3288.c
@@ -1,5 +1,13 @@
-nmcli radio wifi on
-nmcli device wifi rescan
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+system("nmcli device wifi hotspot ifname wlan0 con-name MyHostspot ssid MyHostspotSSID password 12345678");
+system("nmcli connection modify MyHostspot ipv4.addresses 192.168.1.1/24");
 //nmcli dev wifi list
-nmcli dev wifi connect MyCafe password wireless-password
+system("nmcli connection modify MyHostspot ipv4.method shared");
+
+system("nmcli connection up MyHostspot");
+}
 
